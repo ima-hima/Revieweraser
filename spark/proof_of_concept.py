6bb1c58e5aa2f65a7d463c74acf0fe6ed2cdd7eb
Eric Ford <ericford@mac.com>
2019-01-22 17:49:09 -0500

    First push to development branch. Added proof-of-concept code, which now needs to be pulled into Spark.

---
+++ spark/proof_of_concept.py
@@ -0,0 +1,26 @@
+''' Proof of concept for reading in file and dumping it into a Python dict. For this to be paralellizable something will probably
+need to be done at the DB end of things.
+This is expecting a .tsv file in the next directory up, with user_id at index 0, star rating at 7 and review body at 13. '''
+
+camin = open('newCams.tsv')
+
+users = dict()
+line = camin.readline()
+for line in camin:
+    line = line.split('\t')
+    # For each user, store how many reviews they gave, the star rating of each, and how many words are in each.
+    try:
+        users[line[1]][0] += 1
+        users[line[1]][1] += int(line[7])
+        users[line[1]][2] += line[13].count(' ')
+    except:
+        users[line[1]] = [1, int(line[7]), line[13].count(' ')]
+
+multiple = 0 # Will tell me how many will be eliminated.
+             # In this case, two or more reviews that are 5 stars and have five or fewer words.
+for user in users:
+    if users[user][0] > 1 and users[user][1] / users[user][0] == 5 and users[user][2] / users[user][0] < 6:
+        multiple += 1
+        print(user, users[user][0], users[user][1] / users[user][0], users[user][2] / users[user][0])
+
+print( len(users), multiple )
