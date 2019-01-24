2d01b4a4e2f5f4816360be71e6fbe01ae3c2a650
ima-hima <ubuntu@ip-10-0-0-4.ec2.internal>
2019-01-24 03:19:54 +0000

    Okay, NOW it's running through Spark. Output is correct. next step: Cassandra.

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
 cd spark
+# spark-submit \
+#     --jars /home/ubuntu/Insight-DE-2019A-Project/pyspark-cassandra-0.9.0.jar \
+#     --py-files /home/ubuntu/Insight-DE-2019A-Project/pyspark-cassandra-0.9.0.jar \
+#     --conf spark.cassandra.connection.host=ec2-52-206-18-180.compute-1.amazonaws.com \
+#     --master spark://52.206.18.180:7077 \
+#     spark_run.py
+
 spark-submit spark_run.py
