df2cac8ce468a387c160bee0d1fa46014624e786
Eric Ford <ericford@mac.com>
2019-02-05 10:58:20 -0500

    Added sliders to widget, started on actual JS/DOM injection, realized I'll probably want to use JQuery.

--- src/Review-hide_extension/popup.js
+++ src/Review-hide_extension/popup.js
@@ -18,3 +18,28 @@ changeColor.onclick = function(element) {
         {code: 'document.body.style.backgroundColor = "' + color + '";'});
   });
 };
+var minSlider = document.getElementById("min");
+var minOutput = document.getElementById("minVal");
+minVal.innerHTML = minSlider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+minSlider.oninput = function() {
+  minVal.innerHTML = this.value;
+}
+
+var maxSlider = document.getElementById("max");
+var maxOutput = document.getElementById("maxVal");
+maxVal.innerHTML = maxSlider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+maxSlider.oninput = function() {
+  maxVal.innerHTML = this.value;
+}
+
+var reviewers = document.getElementsByClassName("a-profile");
+
+for (var i = 0, max = reviewers.length; i < max; i++) {
+     if (reviewers[i].href.includes("AGLHTJEJHQQ763RAURZ2SRP2VKBA")) {
+       reviewers[i].style.backgroundColor = "blue"
+     }
+}
