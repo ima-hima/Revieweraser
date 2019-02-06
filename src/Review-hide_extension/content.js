3e4b95322b55ac48fee796311e295513c82dba88
Eric Ford <ericford@mac.com>
2019-02-06 13:55:52 -0500

    Added content script.

---
+++ src/Review-hide_extension/content.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// chrome.runtime.onInstalled.addListener(function() {
+//   chrome.storage.sync.set({color: '#3aa757'}, function() {
+//     console.log("The color is green.");
+//   });
+//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+//       chrome.declarativeContent.onPageChanged.addRules([{
+//         conditions: [new chrome.declarativeContent.PageStateMatcher({
+//           pageUrl: {hostEquals: 'www.amazon.com'},
+//         })
+//         ],
+//             actions: [new chrome.declarativeContent.ShowPageAction()]
+//       }]);
+//   });
+// });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    alert(request.greeting);
+    sendResponse({farewell: "goodbye"});
+    // return true;
+  }
+);
