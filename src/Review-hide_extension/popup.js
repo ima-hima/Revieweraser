62ff0a76ff2c3ab3502c914ae37b3613fa89dea7
ima-hima <ubuntu@ip-10-0-0-4.ec2.internal>
2019-02-20 03:28:30 +0000

    Revert "Extension is go."
    This reverts commit 1591a0cbbc833926a72a6ad7f4566f639a8f95e8.

--- src/Review-hide_extension/popup.js
+++ src/Review-hide_extension/popup.js
@@ -15,55 +15,57 @@ $(document).ready(function() {
   var wordSlider      = $("#wordSlider");
   var wordCount       = $("#wordCount");
   var lowReviewCheck  = $("#lowReviewCheck");
-  var highReviewCheck = $("#highReviewCheck");
+  var highReviewcheck = $("#highReviewcheck");
   var wordCountCheck  = $("#wordCountCheck");
 
   minOutput.innerHTML = minSlider.value; // Display the default slider value
   maxOutput.innerHTML = maxSlider.value;
   wordCount.innerHTML = wordSlider.value;
 
-  // Check for button selection
-  $("#lowReviewCheck").on('input', function() {
-    console.log($(this));
-    sendToContent('lowReviewCheck', $(this).is(':checked'), minSlider.val())
-  });
+  // $('.slider').slider({ disabled: true });
 
-  $("#highReviewCheck").on('input', function() {
-    sendToContent('highReviewCheck', $(this).is(':checked'), maxSlider.val())
-  });
-
-  $("#wordCountCheck").on('input', function() {
-    sendToContent('wordCountCheck', $(this).is(':checked'), wordSlider.val())
-  });
+  // wordCountCheck.oninput = function() {
+  //     wordSlider.toggle();
+  // }
 
 
-  // Now update the slider values
+
+  // Now updated the slider values
   // First with min value
   $("#minSlider").on('input', function() {
     // Show min value in display
     $("#curMinVal").html($(this).val());
-    sendToContent('curMinVal', lowReviewCheck.is(':checked'), $(this).val())
+    sendToContent('#curMinVal', $(this).val())
   });
 
   // Same thing, now with max value
   $("#maxSlider").on('input', function() {
     // Show min value in display
     $("#curMaxVal").html($(this).val());
-    sendToContent('curMaxVal', highReviewCheck.is(':checked'), $(this).val())
+    sendToContent('#curMaxVal', $(this).val())
   });
+
 
   // Finally word slider
   $("#wordSlider").on('input', function() {
     // Show min value in display
     $("#wordCount").html($(this).val());
-    sendToContent('wordCount', wordCountCheck.is(':checked'), $(this).val())
+    sendToContent('#wordCount', $(this).val())
   });
+
+
+  //   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //   chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+  //     console.log(response.farewell);
+  //   });
+  // });
+
 });
 
-function sendToContent(which, checkbox, sliderVal) {
+function sendToContent(which, val) {
     // Send to content.js. Note that which is a jQuery selector.
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {selector: which, checkbox: checkbox, sliderVal: sliderVal}, function(response) {
+      chrome.tabs.sendMessage(tabs[0].id, {selector: which, val: val}, function(response) {
         console.log(response.farewell);
       });
     });
